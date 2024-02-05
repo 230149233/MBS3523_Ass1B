7dc86b8757b01d0df97ce210e2a332a13d4c6dcf
230149233 <158991960+230149233@users.noreply.github.com>
2024-02-05 14:51:12 +0800

    Add files via upload

---
+++ MBS3523_Assignment_1bQ3.py
@@ -0,0 +1,21 @@
+import cv2
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    width = cam.get(3)
+    height = cam.get(4)
+
+    frame_2 = cv2.flip(frame,1)
+
+    top_row = cv2.hconcat([frame,frame_2])
+    bottom_row = cv2.flip(top_row,0)
+    combine_frame = cv2.vconcat([top_row,bottom_row])
+
+    cv2.imshow('Frame',combine_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
